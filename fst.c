610e0abdc01ac1759868cbaebf6a900c34295687
nitinmudgal7 <100934962+nitinmudgal7@users.noreply.github.com>
2022-05-27 22:17:33 +0530

    My First Commit

---
+++ fst.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char name [30];
+
+    printf("What's you Name ?\n"); 
+    scanf("%s", name);
+    printf("Hello, %s have a great day ahead\n", name);
+    return 0;
+
+}
