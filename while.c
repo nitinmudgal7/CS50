5e10d0f0e24386208607fafff83fc095dd58faec
nitinmudgal7 <100934962+nitinmudgal7@users.noreply.github.com>
2022-05-31 08:24:54 +0530

    new bit

---
+++ while.c
@@ -0,0 +1,11 @@
+# include <stdio.h>
+int main (void)
+{
+    int counter = 1;
+    while (counter < 4)
+    {
+        printf("Meaow %i \n", counter);
+        counter ++ ;
+    }
+    return 0;
+}
---
+++ wrk.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, k;
+    i = 11;
+    k = i%3; 
+    printf("%i", k);
+    return 0;
+}
